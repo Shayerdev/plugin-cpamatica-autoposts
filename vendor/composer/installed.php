1a69ed05756d9d746b9626d1a5dfd96eed6f3b67
Chemy Development <imjuniordeveloper@gmail.com>
2023-11-02 18:16:00 +0200

    Init repozitory

---
+++ vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'cpamatica/autoposts',
+  ),
+  'versions' => 
+  array (
+    'cpamatica/autoposts' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'squizlabs/php_codesniffer' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '3.x-dev',
+      ),
+      'reference' => '7763e2e1f773cb0615ed8afa133189fc804f583d',
+    ),
+  ),
+);
