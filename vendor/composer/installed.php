9c0ce0030fd9d2432a8d661e8b46709a67e2fec1
Chemy Development <imjuniordeveloper@gmail.com>
2023-11-06 07:03:16 +0200

    vendor requirements

---
+++ vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '472ccf9be82429533138ed88e05f0b8c256f056a',
+    'name' => 'cpamatica/autoposts',
+  ),
+  'versions' => 
+  array (
+    'cpamatica/autoposts' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '472ccf9be82429533138ed88e05f0b8c256f056a',
+    ),
+    'squizlabs/php_codesniffer' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '3.x-dev',
+      ),
+      'reference' => '7763e2e1f773cb0615ed8afa133189fc804f583d',
+    ),
+  ),
+);
