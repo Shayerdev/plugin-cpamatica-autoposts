5344b8669953e08e1cb5242badf84c2bbec4e60e
Chemy Development <imjuniordeveloper@gmail.com>
2023-11-06 15:14:20 +0200

    Delete model abstraction

--- src/includes/abstractions/models/DatabaseFields.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace CAP\includes\abstractions\models;
-
-abstract class DatabaseFields
-{
-    abstract protected static function getField();
-    abstract protected static function setField();
-}
